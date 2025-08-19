03a5366fefb85811831c029cc45ab9723edf5052
Sanjula21 <sanjulathilan6@gmail.com>
2025-08-19 19:58:46 +1000

    adding comments
    Added comments to Enrolment.js

--- Server/models/Enrolment.js
+++ Server/models/Enrolment.js
@@ -1,10 +1,16 @@
+// Import mongoose to work with MongoDB
 const mongoose = require('mongoose');
 
+// Create schema for enrolment form
 const enrolmentSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    message: String,
-    date: { type: Date, default: Date.now }
+    name: String,        
+    email: String,       
+    message: String,     
+    date: {         
+        type: Date,
+        default: Date.now
+    }
 });
 
+// Export the model so it can be used in other files
 module.exports = mongoose.model('Enrolment', enrolmentSchema);
